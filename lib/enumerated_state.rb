fdb76d8279fdaab451820123aea28b53d75e4116
jeffp <jpatmon@gmail.com>
2010-05-29 10:17:27 -0700

    1.8.7 localjumperror fixed

--- lib/enumerated_state.rb
+++ lib/enumerated_state.rb
@@ -11,7 +11,6 @@ module EnumeratedState
 end
 
 class Class
-
   def acts_as_enumerated_state(enum_attr, opts={})
     enum_attr = enum_attr.to_sym
     unless (self.respond_to?(:has_enumerated_attribute?) && self.has_enumerated_attribute?(enum_attr))
@@ -28,22 +27,25 @@ class Class
 
     define_chained_method(:write_enumerated_attribute, enum_attr) do |attribute, value|
       without_method = "write_enumerated_attribute_without_#{enum_attr}".to_sym
-      return self.__send__(without_method, attribute, value) unless enum_attr == attribute.to_sym
 
-      old_value = self.read_enumerated_attribute(enum_attr)
-      result = self.__send__(without_method, attribute, value)
+      unless enum_attr == attribute.to_sym
+        self.__send__(without_method, attribute, value)
+      else
+        old_value = self.read_enumerated_attribute(enum_attr)
+        result = self.__send__(without_method, attribute, value)
 
-      return result if value == old_value
-      unless old_value.nil?
-        _module = self.class.get_module_for_enumerated_state(attribute, old_value)
-        self.unmix(_module) if _module
+        unless value == old_value
+          unless old_value.nil?
+            _module = self.class.get_module_for_enumerated_state(attribute, old_value)
+            self.unmix(_module) if _module
+          end
+          unless value.nil?
+            _module = self.class.get_module_for_enumerated_state(attribute, value)
+            self.mixin(_module) if _module
+          end
+        end
+        result
       end
-      unless value.nil?
-        _module = self.class.get_module_for_enumerated_state(attribute, value)
-        self.mixin(_module) if _module
-      end
-
-      result
     end
   end
   alias_method :enumerated_state_pattern, :acts_as_enumerated_state
